e03cbc534c5b220c2c225e351092f2958d5e3ba0
coderyy <kt1229379984@qq.com>
2021-01-30 15:48:44 +0800

    完成前端登陆注册界面

---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,11 @@
+import { defineComponent } from 'vue';
+import { UserOutlined, DisconnectOutlined, LockOutlined } from '@ant-design/icons-vue';
+
+export default defineComponent({
+  components: {
+    UserOutlined, DisconnectOutlined, LockOutlined
+  },
+  setup() {
+
+  }
+});
---
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="auth">
+    <!-- 背景图 -->
+    <div class="bg"></div>
+    <!-- 标题 -->
+    <div class="title-info">
+      <img src="https://ncstatic.clewm.net/rsrc/2020/1016/02/4757e4910cb527fc040d019a93ded74f.png?x-oss-process=image/resize,w_750/format,gif/sharpen,100/quality,Q_80/interlace,1/auto-orient,1" alt="">
+      <div class="title">图书管理系统 后台</div>
+    </div>
+
+    <!-- 表单 -->
+    <div class="form">
+      <a-tabs @change="callback">
+        <!-- 登陆部分 -->
+        <a-tab-pane key="1" tab="登陆">
+          <div class="item">
+            <a-input placeholder="用户名" size="large" allow-clear>
+              <template v-slot:prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-input-password size="large"  placeholder="密码" />
+          </div>
+
+          <div class="item">
+            <a href="">忘记密码</a>
+          </div>
+
+          <div class="item">
+            <a-button type="primary">登陆</a-button>
+          </div>
+        </a-tab-pane>
+
+        <!-- 注册部分 -->
+        <a-tab-pane key="2" tab="注册">
+          <div class="item">
+            <a-input placeholder="用户名" size="large" allow-clear>
+              <template v-slot:prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-input-password size="large"  placeholder="密码" />
+          </div>
+
+          <div class="item">
+            <a-input-password size="large"  placeholder="确认密码" />
+          </div>
+
+          <div class="item">
+            <a-input placeholder="邀请码" size="large" allow-clear>
+              <template v-slot:prefix>
+                <DisconnectOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-button type="primary">注册</a-button>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+<script src="./index.js"></script>
+
+<style lang="scss" scoped>
+  @import './index.scss';
+</style>
